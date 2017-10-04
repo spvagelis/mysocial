abed1e1e4a511569b7598bc31d965e8636cfb144
vagelis spirou <vagelisspirou@MacBook-Pro.local>
2017-10-04 19:13:50 +0300

    Completed email auth

--- mysocial/SignInVC.swift
+++ mysocial/SignInVC.swift
@@ -12,6 +12,11 @@ import FBSDKLoginKit
 import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: FancyField!
+    
+    @IBOutlet weak var pwdField: FancyField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -61,6 +66,36 @@ class SignInVC: UIViewController {
             
         
     }
+    
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = pwdField.text {
+            
+          Auth.auth().signIn(withEmail: email, password: pwd, completion: { (user, error) in
+            if error == nil {
+                
+                print("Vageli: Email user authenticated with Firebase")
+                
+            } else {
+                
+                Auth.auth().createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                    if error != nil {
+                        print("Vageli: Unable to authenticate with Firebase using email")
+                    } else {
+                        
+                        print("Vageli: Succesfully authenticated with Firebase")
+                        
+                    }
+                })
+                
+            }
+          })
+            
+        }
+        
+    }
+    
 }
 
 
